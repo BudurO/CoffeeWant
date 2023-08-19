a01520149c9da3cfad1ac321a26ac68621d1efc1
BudurO <buduralthubyani@gmail.com>
2023-08-19 20:53:31 +0300

    CoffeeWant Pro

---
+++ CoffeeWant.js
@@ -0,0 +1,87 @@
+
+
+//------------------------------Capuccino-----------------------------------//
+function AnimationCoffeeb() {
+    let cupCoffeeb = document.getElementById("cupCoffeeb");
+    cupCoffeeb.innerText = "Coffee!"
+    cupCoffeeb.style.backgroundColor = "#3F2305"
+
+    cupCoffeeb.style.color = "#FFFFFF"
+    cupCoffeeb.style.borderRadius = "0 0 0 0"
+    cupCoffeeb.style.justifyContent = "center" 
+}
+function AnimationCoffeef() {
+    let cupCoffeef = document.getElementById("cupCoffeeMailk");
+    cupCoffeef.innerText = "Mailk!"
+    cupCoffeef.style.color = "black"
+    cupCoffeef.style.justifyContent = "center" 
+    cupCoffeef.style.backgroundColor = "#F8ECD1"
+
+    cupCoffeef.style.borderRadius = "0 0 11px 11px"
+
+
+}
+
+function Capuccino() {
+    AnimationCoffeeb()
+    AnimationCoffeef()
+    console.log("Capuccino!");
+
+}
+
+//------------------------------Latte-----------------------------------//
+function BlackCoffeeb() {
+    let cupCoffeeb = document.getElementById("cupCoffeeb");
+    cupCoffeeb.style.backgroundColor = "#3F2305"
+    cupCoffeeb.innerText = "Coffee!"
+    cupCoffeeb.style.color = "#FFFFFF"
+    cupCoffeeb.style.borderRadius = "0 0 0 0"
+    cupCoffeeb.style.justifyContent = "center" 
+
+
+
+}
+function MailkCoffeef() {
+    let cupCoffeef = document.getElementById("cupCoffee");
+    cupCoffeef.innerText = "Mailk!"
+    cupCoffeef.style.color = "black"
+    cupCoffeef.style.justifyContent = "center" 
+    cupCoffeef.style.backgroundColor = "#F8ECD1"
+    cupCoffeef.style.borderRadius = "0 0 11px 11px"
+
+
+}
+
+function SteamedMilk() {
+    let cupCoffeesn = document.getElementById("SteamedMilk");
+    cupCoffeesn.innerText = "Steamed!"
+    cupCoffeesn.style.color = "black"
+    cupCoffeesn.style.justifyContent = "center" 
+    cupCoffeesn.style.backgroundColor = "#EAC696"
+    cupCoffeesn.style.borderRadius = "0 0 0 0"
+}
+function Latte() {
+    AnimationCoffeeb()
+    AnimationCoffeef()
+    SteamedMilk()
+    console.log("Latte!");
+
+}
+
+//------------------------------Arabic-----------------------------------//
+function AnimationCoffeebar() {
+    let cupCoffeeb = document.getElementById("cupCoffeeArabic");
+    cupCoffeeb.style.backgroundColor = "#bd8a26"
+    cupCoffeeb.innerText = "Coffee!"
+    cupCoffeeb.style.color = "#FFFFFF"
+    cupCoffeeb.style.borderRadius = "0 0 11px 11px"
+    cupCoffeeb.style.justifyContent = "center" 
+    console.log("Arabic coffee!");
+
+}
+
+//------------------------------Clean-----------------------------------//
+function Clean() {
+    let cupCoffeeb = document.getElementById("cupCoffeeb");
+    let cupCoffeef = document.getElementById("cupCoffeeMailk");
+}
